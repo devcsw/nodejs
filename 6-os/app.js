a9d09fccb41a3139ae1a086be57b5a21a37496a9
devcsw <mbso520@gmail.com>
2021-05-29 08:55:04 +0900

    6-os

---
+++ 6-os/app.js
@@ -0,0 +1,20 @@
+const os = require('os');
+
+console.log(os.EOL === '\r\n');
+
+console.log(os.hostname());
+console.log(os.loadavg());
+console.log(os.uptime());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.cpus());
+console.log(os.type());
+console.log(os.release());
+console.log(os.networkInterfaces());
+console.log(os.homedir());
+console.log(os.userInfo());
+console.log(os.userInfo());
+
+
+
+
