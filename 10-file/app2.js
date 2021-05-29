e99e07f427eafc04648edede10e4b71987a93178
devcsw <mbso520@gmail.com>
2021-05-29 19:09:42 +0900

    10-file-2

---
+++ 10-file/app2.js
@@ -0,0 +1,32 @@
+const fs = require('fs').promises;
+
+// promises 이므로
+//read a file
+
+fs.readFile('./text.txt', 'utf-8')
+.then(data => console.log(data))
+.catch(console.error);
+
+//writing a file
+fs.writeFile('./text.txt', 'hello choi')
+.catch(console.error);
+
+fs.appendFile('./text.txt', 'appendfile')
+// .then(()=> {
+// 비동기식으로 진행하지 않으려면 then 안에서 실행
+// })
+.catch(console.error);
+
+//copy
+fs.copyFile('./file.txt', './file2.txt')
+.catch(console.error);
+
+//folder
+fs.mkdir('sub-folder')
+.catch(console.error);
+
+fs.readdir('./')
+.then(console.log)
+.catch(console.error);
+
+//
