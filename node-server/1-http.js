9d9ad8fbb65546f1c6cec8a9a20ed437ad746945
devcsw <mbso520@gmail.com>
2021-06-02 06:53:59 +0900

    node-server-2

--- node-server/1-http.js
+++ node-server/1-http.js
@@ -1,13 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 //const http2 = require('http2');
 
 const server = http.createServer((req, res)=> {
-    console.log('incoming');
-    console.log(req.headers);
-    console.log(req.httpVersion);
-    console.log(req.method);
-    console.log(req.url);
-    res.write('welcome!');
+    // console.log('incoming');
+    // console.log(req.headers);
+    // console.log(req.httpVersion);
+    // console.log(req.method);
+    // console.log(req.url);
+    const url = req.url;
+    res.setHeader('Content-Type', 'text/html');
+    if(url === '/'){
+            
+        const read = fs.createReadStream('./html/index.html');
+        read.pipe(res);
+    } else if (url === '/courses'){
+        fs.createReadStream('./html/courses.html').pipe(res);
+    } else {
+        fs.createReadStream('./html/not-found.html').pipe(res);
+    
+    }
     res.end();
 }) ;
 
