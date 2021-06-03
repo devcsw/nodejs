064dccede5649186d93723bb7f91551e8df1b310
devcsw <mbso520@gmail.com>
2021-06-03 20:58:06 +0900

    express_cookieparser,morgan,helmet

--- 7-cors/app.js
+++ 7-cors/app.js
@@ -1,24 +1,30 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
-
-app.use(
-    cors({
+const corsOption = {
     origin: ['http://127.0.0.1:5500'],
     optionsSuccessStatus: 200,
     credentials: true,
-    })
-);
+    };
+//middleware 등록
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('combined')); //
+app.use(cors(corsOption)); //서버에 허락해주는 것
+app.use(helmet()); //공통적인 보안에 필요한 것들을 추가해줌
+
 
 app.get('/', (req, res) =>{
-    res.send('Welcome!');
-    // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-    // res.setHeader(
-    //     'Acess-Control-Allow-Methods',
-    //     'OPTIONS, GET, POST, PUT, DELETE'
-    // );
-
+    
+    console.log(req.body);
+    console.log(req.cookies); //undefined 쿠키파서 import
+    console.log(req.cookies.yummy_cookie);
+    res.send('welcome');
 });
 
+//
 app.listen(8080);
