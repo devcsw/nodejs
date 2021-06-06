8403968e7b272c0a4af67374cfe6d3bdd9ec8fc8
devcsw <mbso520@gmail.com>
2021-06-06 19:47:22 +0900

    8-validation

---
+++ 8-validation/app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { body, param, validationResult } from 'express-validator';
+
+const app = express();
+app.use(express.json());
+
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if(errors.isEmpty()) {
+        return next();
+    }
+    return res.status(400).json({message: errors.array() });
+};
+
+app.post(
+    '/users', 
+    [
+    body('name').isLength({min: 2, max: 10}).withMessage('이름은 2글자이상 10글자 이하'),
+    body('age').isInt().withMessage('숫자를 입력해'), 
+    body('email').isEmail().withMessage('이메일을 입력해'),
+    body('job.name').notEmpty,
+    validate,
+    ],
+    (req, res, next) => {
+    //if(req.body.email..){
+    //    res.status(400).send({message: 'email'});
+    //} else if() ....
+    console.log(req.body);
+    res.sendStatus(201);
+});
+
+app.get('/:email',
+    [param('email').isEmail().withMessage('이메일 입력해줏요'), validate],
+    (req, res, next) => {
+    res.send('email');
+});
+
+app.listen(8080);
